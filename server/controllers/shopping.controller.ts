8d0bcbbfd61d8817252c5f94fafcc7ba71c48f47
valentin-gv <valentin.gavela@despegar.com>
2020-01-04 22:28:36 -0300

    add product detail in shopping controller

--- server/controllers/shopping.controller.ts
+++ server/controllers/shopping.controller.ts
@@ -27,6 +27,20 @@ const ShoppingController = {
 
     res.render('main', { stats, title: 'my title', baseurl: url, username });
   },
+  productDetail: async (req: IRequest, res: IResponse) => {
+    const { url, username, params } = req;
+
+    if (params.hasOwnProperty('productId') === false) {
+      res.redirect('/404');
+
+      return;
+    }
+
+    const statsPath = path.resolve(PROJECT_PATH, './dist/stats.json');
+    const stats = require(statsPath);
+
+    res.render('main', { stats, title: 'my title', baseurl: url, username });
+  },
 };
 
 export = ShoppingController;
