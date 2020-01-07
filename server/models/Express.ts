580d2ebb87084b93565323a93fb22722dcf670da
valentin-gv <valentin.gavela@despegar.com>
2020-01-07 01:55:51 -0300

    remove axios instance from request interface

--- server/models/Express.ts
+++ server/models/Express.ts
@@ -1,8 +1,6 @@
 import expressCore = require('express-serve-static-core');
-import { AxiosInstance } from 'axios';
 
 export interface IRequest extends expressCore.Request {
-  apiClient: AxiosInstance;
   username: string;
 }
 
