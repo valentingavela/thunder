6ea556095a68544ee0cd6c5496bc41f64db9a101
valentin-gv <valentin.gavela@despegar.com>
2020-01-07 18:21:20 -0300

    remove unused vars

--- web/constants/global/index.ts
+++ web/constants/global/index.ts
@@ -5,8 +5,4 @@ declare const ___TIMEOUT___: number;
 export const __APP_BASENAME__ = ___APP_BASENAME___;
 export const __IS_DEVELOPMENT__ = ___IS_DEVELOPMENT___;
 export const __TIMEOUT__ = ___TIMEOUT___;
-export const __DATE_FORMAT__ = 'DD/MM/YYYY';
-export const __TIME_FORMAT__ = 'HH:mm';
-export const __DATE_TIME_FORMAT__ = `${__DATE_FORMAT__} ${__TIME_FORMAT__}`;
 export const __IS_PRODUCTION__ = !__IS_DEVELOPMENT__;
-export const __DEFAULT_TIMEZONEID__ = 'America/Argentina/Buenos_Aires';
